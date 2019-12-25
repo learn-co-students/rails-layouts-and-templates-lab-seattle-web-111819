fd0701eec5fb2e02db13cc94ae32b5652864fd94
Alice Won <alicewon4@gmail.com>
2019-12-24 20:29:34 -0800

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,16 @@
+class StoreAdminController < ApplicationController
+  layout "admin"
+
+  def home
+    render :home
+  end
+
+  def orders
+    render action: "orders", layout: "order_administration"
+  end
+
+  def invoice
+    render action: "invoice", layout: false
+  end
+
+end
